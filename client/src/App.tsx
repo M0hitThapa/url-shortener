1ab3aebfedf90a868011653e83d499e8cb4de7d3
Mohit Thapa <thapamohit2058@gmail.com>
2025-01-14 23:10:10 +0545

    .

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+interface IAppProps {}
+
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  );
+};
+
+export default App;
--- server/routes/url.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { handleGenerateNewShortURL } = require('../controllers/url'); // Correctly import from controllers
-
-const router = express.Router();
-
-router.post('/', handleGenerateNewShortURL); // Use the imported function as a callback for the POST route
-
-module.exports = router;
